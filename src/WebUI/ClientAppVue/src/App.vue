d6042933f0e017029a82a2beb98855b9176d5708
Craig Taylor <craig.taylor@hsd.com.au>
2021-03-11 16:01:55 +1100

    first commit of vue

---
+++ src/WebUI/ClientAppVue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <NavBar />
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import NavBar from "@/components/NavBar.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    NavBar
+  }
+});
+</script>
+<style lang="scss">
+/* You can add global styles to this file, and also import other style files */
+
+/* Provide sufficient contrast against white background */
+a {
+  color: #0366d6;
+}
+
+code {
+  color: #e01a76;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #1b6ec2;
+  border-color: #1861ac;
+}
+</style>
---
+++ src/WebUI/ClientAppVue/src/components/NavBar.vue
@@ -0,0 +1,92 @@
+<template>
+  <header>
+    <nav
+      class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3"
+    >
+      <div class="container">
+        <router-link class="navbar-brand" to="/"
+          >CleanArchitecture.WebUI</router-link
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target=".navbar-collapse"
+          aria-label="Toggle navigation"
+          :aria-expanded="isExpanded"
+          @click="toggle()"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse"
+          :style="{
+            show: isExpanded
+          }"
+        >
+          <!-- <app-login-menu></app-login-menu> -->
+          <ul class="navbar-nav flex-grow">
+            <li class="nav-item">
+              <router-link class="nav-link text-dark" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link text-dark" to="/"
+                >Counter</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link text-dark" to="/"
+                >Fetch data</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link text-dark" to="/">To-Do</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="/api" target="_blank">API</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+  setup() {
+    let isExpanded = false;
+
+    function collapse() {
+      isExpanded = false;
+    }
+    function toggle() {
+      isExpanded = !isExpanded;
+    }
+
+    return { collapse, isExpanded, toggle };
+  }
+});
+</script>
+<style lang="scss" scoped>
+a.navbar-brand {
+  white-space: normal;
+  text-align: center;
+  word-break: break-all;
+}
+
+html {
+  font-size: 14px;
+}
+@media (min-width: 768px) {
+  html {
+    font-size: 16px;
+  }
+}
+
+.box-shadow {
+  box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.05);
+}
+</style>

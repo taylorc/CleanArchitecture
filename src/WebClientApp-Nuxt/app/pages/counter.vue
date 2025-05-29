4db9924003885fe3cfffdf929c80cae4b198974e
taylorc <taylorc5150@gmail.com>
2025-05-29 20:48:57 +1000

    counter component built

--- src/WebClientApp-Nuxt/app/pages/counter.vue
+++ src/WebClientApp-Nuxt/app/pages/counter.vue
@@ -1,3 +1,20 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const currentCount = ref(0);
+
+function incrementCounter() {
+  currentCount.value++;
+}
+</script>
 <template>
-    <h2>Counter</h2>
+    <div>
+        <h1>Counter</h1>
+
+        <p>This is a simple example of a Nuxt component.</p>
+
+        <p aria-live="polite">Current count: <strong>{{currentCount}}</strong></p>
+
+        <button class="btn btn-primary" @click="incrementCounter">Increment</button>
+      </div>
 </template>
